f53981726060baacf2372c2ce901cc6c43c3cba8
DewiPuspitass <145330132+DewiPuspitass@users.noreply.github.com>
2025-06-12 20:13:01 +0700

    Menyembunyikan profile jika belum login di settings

--- app/src/main/java/com/dewipuspitasari0020/laventry/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/dewipuspitasari0020/laventry/ui/screen/SettingsScreen.kt
@@ -17,16 +17,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.dewipuspitasari0020.laventry.model.User
 import com.dewipuspitasari0020.laventry.navigation.Screen
+import com.dewipuspitasari0020.laventry.network.UserDataStore
 import com.dewipuspitasari0020.laventry.ui.theme.LaventryTheme
 import com.dewipuspitasari0020.laventry.ui.theme.bg
 import com.dewipuspitasari0020.laventry.ui.theme.blue
@@ -44,6 +48,13 @@ fun SettingsScreen(navController: NavHostController) {
         Screen.Settings.route -> 3
         else -> -1
     }
+
+    val context = LocalContext.current
+    val datastore = UserDataStore(context)
+    val user by datastore.userFlow.collectAsState(User())
+
+    val isLoggedIn = user.email.isNotEmpty()
+
     Scaffold(
         containerColor = bg,
         topBar = {
@@ -79,23 +90,25 @@ fun SettingsScreen(navController: NavHostController) {
             )
         },
     ){ innerPadding ->
-        ScreenContentSettings(modifier = Modifier.padding(innerPadding), navController = navController)
+        ScreenContentSettings(modifier = Modifier.padding(innerPadding), navController = navController, isLoggedIn = isLoggedIn)
     }
 }
 
 @Composable
-fun ScreenContentSettings(modifier: Modifier = Modifier, navController: NavHostController) {
+fun ScreenContentSettings(modifier: Modifier = Modifier, navController: NavHostController, isLoggedIn: Boolean) {
     Column(
         modifier = modifier
             .padding(16.dp)
             .fillMaxSize()
     ) {
-        CardInfo(
-            onClick = {
-                navController.navigate(Screen.Profile.route)
-            },
-            label = "Profile"
-        )
+        if (isLoggedIn) {
+            CardInfo(
+                onClick = {
+                    navController.navigate(Screen.Profile.route)
+                },
+                label = "Profile"
+            )
+        }
         CardInfo(
             onClick = {
                 navController.navigate(Screen.InfoAplikasi.route)
